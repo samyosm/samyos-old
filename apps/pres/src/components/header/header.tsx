9d98bbb74585fdb1acdc6e06188147d6a36f5679
Samy Rahmani <samy.osmium@gmail.com>
2023-01-29 10:47:15 -0500

    chore: remove prettier

--- apps/pres/src/components/header/header.tsx
+++ apps/pres/src/components/header/header.tsx
@@ -14,17 +14,26 @@ export default component$(() => {
       </div>
       <ul>
         <li>
-          <a href="https://qwik.builder.io/docs/components/overview/" target="_blank">
+          <a
+            href="https://qwik.builder.io/docs/components/overview/"
+            target="_blank"
+          >
             Docs
           </a>
         </li>
         <li>
-          <a href="https://qwik.builder.io/examples/introduction/hello-world/" target="_blank">
+          <a
+            href="https://qwik.builder.io/examples/introduction/hello-world/"
+            target="_blank"
+          >
             Examples
           </a>
         </li>
         <li>
-          <a href="https://qwik.builder.io/tutorial/welcome/overview/" target="_blank">
+          <a
+            href="https://qwik.builder.io/tutorial/welcome/overview/"
+            target="_blank"
+          >
             Tutorials
           </a>
         </li>
--- apps/pres/src/routes/index.tsx
+++ apps/pres/src/routes/index.tsx
@@ -2,11 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return (
-    <div>
-      Hello, world!
-    </div>
-  );
+  return <div>Hello, world!</div>;
 });
 
 export const head: DocumentHead = {
