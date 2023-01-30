c949858de0bb3f312dbde7cdd11d82d7213b1980
Samy Rahmani <samy.osmium@gmail.com>
2023-01-29 20:31:16 -0500

    fix(tooltastic): tailwind not working

--- apps/tooltastic/src/routes/layout.tsx
+++ apps/tooltastic/src/routes/layout.tsx
@@ -1,5 +1,6 @@
 import { component$, Slot } from '@builder.io/qwik';
 import { Sidebar } from '../components/sidebar/Sidebar';
+import '../global.css';
 
 export default component$(() => {
   return (
