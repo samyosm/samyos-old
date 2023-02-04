32e92325f35fe9759e929c56cf0a1a1e33b7da73
Samy Rahmani <samy.osmium@gmail.com>
2023-02-04 18:09:34 -0500

    basics

--- apps/tooltastic/src/App.tsx
+++ apps/tooltastic/src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="w-full h-full bg-gradient-to-b from-gray-900 to-gray-600 flex text-white">
+      <nav className="w-full max-w-sm border-r">
+        <h2 className="p-5">ToolTastic</h2>
+        <hr />
+      </nav>
+
+      <main className="w-full">
+        <h1>FileBin</h1>
+        <p>Easily share files without registration</p>
+        
+      </main>
     </div>
   )
 }
