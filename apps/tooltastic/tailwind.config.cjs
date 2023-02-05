7ba5b24d77f1ee187b4b62c2e2dea97adb1cd4e5
Samy Rahmani <samy.osmium@gmail.com>
2023-02-05 10:50:45 -0500

    feat(tooltastic): added UI

--- apps/tooltastic/tailwind.config.cjs
+++ apps/tooltastic/tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    './node_modules/tailwind-datepicker-react/dist/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
