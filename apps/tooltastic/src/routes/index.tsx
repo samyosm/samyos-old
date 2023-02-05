9cae1d5b35f327adeba15ba54c57b57d966d214c
Samy Rahmani <samy.osmium@gmail.com>
2023-02-05 18:31:40 -0500

    changed text

--- apps/tooltastic/src/routes/index.tsx
+++ apps/tooltastic/src/routes/index.tsx
@@ -45,7 +45,7 @@ export default component$(() => {
               />
             </div>
             <div class="flex flex-col gap-2">
-              <h5 class="">2. Choose expiration</h5>
+              <h5 class="">2. Choose when the file will expire</h5>
               <select
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 required={true}
