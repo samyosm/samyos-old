fbd1191c7ad47bc4f4dcf969544bb28b54daa38e
Samy Rahmani <samy.osmium@gmail.com>
2023-02-05 21:47:42 -0500

    started cleaning

---
+++ apps/tooltastic/src/components/flowbite/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { component$, Slot } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
+
+export const SidebarLogo = component$(({ name }: { name: string }) => (
+  <div class="flex items-center mb-5">
+    <h4 class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+      {name}
+    </h4>
+  </div>
+));
+
+export const SidebarItem = component$(
+  ({ name, description }: { name: string; description: string }) => {
+
+    return (
+      <Link
+        href={`/${name.toLowerCase()}`}
+        class={` dark:border-gray-600 dark:hover:bg-gray-700 hover:bg-gray-200 cursor-pointer flex flex-col p-4 rounded-md border border-gray-300`}
+      >
+        <h5>{name}</h5>
+        <p class="text-sm">{description}</p>
+      </Link>
+    );
+  }
+);
+
+export const Sidebar = component$(() => {
+
+  return (
+    <nav class="w-fit ">
+      <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+        <Slot name="logo" />
+        <div class="space-y-2">
+          <input
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            type="text"
+            placeholder="Search tool..."
+          />
+          <Slot />
+        </div>
+      </div>
+    </nav>
+  );
+});
--- apps/tooltastic/src/routes/layout.tsx
+++ apps/tooltastic/src/routes/layout.tsx
@@ -1,12 +1,19 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot } from "@builder.io/qwik";
+import { Sidebar, SidebarItem, SidebarLogo } from "~/components/flowbite/Sidebar";
 
 export default component$(() => {
+
   return (
     <>
-      <main>
-        <section>
-          <Slot />
-        </section>
+      <main class="w-full h-full flex bg-gray-50 dark:bg-gray-900">
+        <Sidebar>
+          <SidebarLogo name="ToolTastic" q:slot="logo" />
+          <SidebarItem
+            name="Filebin"
+            description="Easily share files without registration"
+          />
+        </Sidebar>
+        <Slot />
       </main>
     </>
   );
