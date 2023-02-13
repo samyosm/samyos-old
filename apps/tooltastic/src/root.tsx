9c8c3387884731b718e61bdad3263f842aa2106d
Samy Rahmani <samy.osmium@gmail.com>
2023-02-13 16:22:47 -0500

    tweet-bio

--- apps/tooltastic/src/root.tsx
+++ apps/tooltastic/src/root.tsx
@@ -6,6 +6,9 @@ import {
 } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
 
+import '../../tooltastic-api/clients/OpenAI';
+import '../../tooltastic-api/clients/Redis';
+
 import globalStyles from './global.css?inline';
 
 export default component$(() => {
--- apps/tooltastic/src/routes/index.tsx
+++ apps/tooltastic/src/routes/index.tsx
@@ -1,6 +1,5 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
-import cityPlan from '@qwik-city-plan';
 
 export const onGet: RequestHandler = async ({ response }) => {
-  throw response.redirect(cityPlan.routes[1][3]);
+  throw response.redirect('/filebin/');
 };
